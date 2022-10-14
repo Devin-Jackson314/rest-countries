5cdeadee595283af6763c8418fd7ef9871c5342d
Devin-Jackson314 <you@example.com>
2022-10-14 11:25:15 -0400

    got the languages showing now for the currencies

--- src/app/components/countriesfull/countriesfull.component.ts
+++ src/app/components/countriesfull/countriesfull.component.ts
@@ -9,13 +9,22 @@ import { DataService } from 'src/app/services/data.service';
 export class CountriesfullComponent implements OnInit {
   data: any;
   countries: any;
+  language: any;
+  stringObject: any;
+  languages: any;
 
   constructor(private dataservice: DataService) { }
 
   ngOnInit(): void {
      this.data = this.dataservice.getDataProp(['region', 'subregion', 'population', 'flags', 'capital', 'name', 'nativename', 'tld', 'currencies', 'languages', 'borders']).subscribe(responseData => {
-      this.countries = responseData;
-      console.log(responseData);
+       this.countries = responseData;
+       console.log(this.countries)
+      //  this.language = this.dataservice.getDataProp(['languages']).subscribe(responseData => {
+      //    this.languages = responseData
+        
+      //    console.log(this.languages);
+      //  })
+    
     })
   }
 
