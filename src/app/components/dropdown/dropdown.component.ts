ced2c2b6a0025259afca9005a7b5eb52926e2969
Devin-Jackson314 <you@example.com>
2022-10-16 18:54:42 -0400

    added the drop down needs styling though

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -11,5 +11,19 @@ export class DropdownComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+   myFunction() {
+  document.getElementById("myDropdown")?.classList.toggle("show");
+}
+  dropdown(event: any) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 }
